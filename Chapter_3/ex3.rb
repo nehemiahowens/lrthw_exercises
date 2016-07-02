11dce94496bfe5fb2e495cfa9be8fc8811e5c33d
Neheniah Owens <nehemiahowens@outlook.com>
2016-07-01 18:42:54 -0600

    [120477497] Chapter_3

---
+++ Chapter_3/ex3.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+puts 'I will now count my chickens:'
+
+puts "Hens #{(25.0 + 30.0) / 6.0}"
+puts "Roosters #{(100 - 25) * 3.0 % 4}"
+
+puts 'Is it true that 3.0 + 2.0 < 5.0 - 7.0?'
+
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+puts "Oh that's why it's false."
+
+puts 'How about some more.'
+
+puts "Is it greater #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
