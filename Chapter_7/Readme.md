9a904f8e1b32f613cbfb06385554f394ce4736b9
Neheniah Owens <nehemiahowens@outlook.com>
2016-08-15 19:24:56 -0600

    [120477505] Chapter_7 files

---
+++ Chapter_7/Readme.md
@@ -0,0 +1,15 @@
+    (120477505_chapter_7_master) Neheniah Owens
+    Nehemiahs-MacBook-Pro:Chapter_7 $ ruby ex7.rb
+    Mary had a little lamb.
+    It's fleece was white as snow.
+    And everywhere that Marry went.
+    ..........
+    CheeseBurger
+
+# Study Drills:
+
+## 1) Add comments to ex7.rb
+see ex7.rb
+
+## 2.) Please explain the difference between puts and print in your own words in the Readme.md.
+It appears that the difference between the two is the line formatting; e.g. puts creates a new line and print can have multiple lines in succession.
---
+++ Chapter_7/ex7.rb
@@ -0,0 +1,43 @@
+#!usr/bin/env ruby
+
+# Sting for "Mary had a little lamb"
+puts "Mary had a little lamb."
+# String for "It's fleece was white as snow"
+puts "It's fleece was white as #{'snow'}."
+# String for "And everywhere that Marry went"
+puts "And everywhere that Marry went."
+# Multiplies the String . ten times e.g. ..........
+puts "." * 10 # what'd that do?
+
+# Assigning string "C" to variabe end1
+end1 = "C"
+# Assigning string "h" to variabe end2
+end2 = "h"
+# Assigning string "e" to variabe end3
+end3 = "e"
+# Assigning string "e" to variabe end4
+end4 = "e"
+# Assigning string "s" to variabe end5
+end5 = "s"
+# Assigning string "e" to variabe end6
+end6 = "e"
+# Assigning string "B" to variabe end7
+end7 = "B"
+# Assigning string "u" to variabe end8
+end8 = "u"
+# Assigning string "r" to variabe end9
+end9 = "r"
+# Assigning string "g" to variabe end10
+end10 = "g"
+# Assigning string "e" to variabe end11
+end11 = "e"
+# Assigning string "r" to variabe end12
+end12 = "r"
+
+
+# watch that print vs. puts on this line what's it do?
+
+# Creating lines with variables "end1, 2, 3, 4, 5, 6" to show a whole string as Cheese
+print end1 + end2 + end3 + end4 + end5 + end6
+## Creating lines with variables "end7, 8, 9, 10, 11, 12" to show a whole string as Burger
+puts end7 + end8 + end9 + end10 + end11 + end12
